f9959b46471b0dda41e0238887418bb1d2c4687e
McMahon <McMahonM@dfo-mpo.ca>
2019-08-30 10:48:57 -0300

    changes ds_all behaviour

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,11 @@
 .onAttach <- function(libname, pkgname) {
+  
+  assign("ds_all", load_datasources(), envir = .GlobalEnv)
   localVer = utils::packageDescription('Mar.datawrangling')$Version
   packageStartupMessage(paste0("Version: ", localVer))
 }
 
 .onLoad <- function(libname, pkgname){
   options(stringsAsFactors = FALSE)
-  assign("ds_all", load_datasources(), envir = .GlobalEnv)
   Mar.utils::updateCheck(gitPkg = 'Maritimes/Mar.datawrangling')
 }
