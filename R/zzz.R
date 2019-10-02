315a7bc92fd30a422e66177ab134c590f4bd1123
McMahon <McMahonM@dfo-mpo.ca>
2019-10-02 16:09:28 +0100

    fixes issue with corruped documentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,13 @@
 .onAttach <- function(libname, pkgname) {
+  #onAttach is for interactive sessions
   assign("ds_all", Mar.datawrangling::load_datasources(), envir = .GlobalEnv)
+  Mar.utils::updateCheck(gitPkg = 'Maritimes/Mar.datawrangling')
   localVer = utils::packageDescription('Mar.datawrangling')$Version
   packageStartupMessage(paste0("Version: ", localVer))
+  packageStartupMessage("Timezone set to 'America/Halifax' to match data")
 }
 
 .onLoad <- function(libname, pkgname){
   options(stringsAsFactors = FALSE)
   Sys.setenv(TZ = "America/Halifax")
-  packageStartupMessage(paste0("Timezone set to 'America/Halifax' to match data"))
-  Mar.utils::updateCheck(gitPkg = 'Maritimes/Mar.datawrangling')
 }
